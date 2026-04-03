513f3189bf018927cebd7ed59cb4d3f52134fc53
Debbie Yuen <deborahyuen2016@berkeley.edu>
2026-04-02 17:01:11 -0700

    standing robot with h1

--- scripts/discard/unitree_robot.py
+++ scripts/discard/unitree_robot.py
@@ -1,7 +1,10 @@
 from isaacsim import SimulationApp
 simulation_app = SimulationApp({"headless": False})
 
+# import numpy as np
 from isaacsim.core.api import World
+from isaacsim.core.utils.stage import add_reference_to_stage
+from isaacsim.core.prims import Articulation
 from unitree import Unitree
 
 world = World()
@@ -14,6 +17,18 @@ robot = Unitree(
 )
 
 world.reset()
+
+art = Articulation("/World/Unitree")
+art.initialize()
+
+# print joints
+# joint_names = art.get_joint_names()
+# print("Joint names:")
+# for i, name in enumerate(joint_names):
+#     print(i, name)
+
+# standing_pos = [0.0] * art.num_dof  # replace with standing pose
+# art.set_joint_positions(standing_pos)
 
 while simulation_app.is_running():
     world.step(render=True)
